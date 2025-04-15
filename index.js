072d4edd629f343b31a1cf6504de129a8d536d5e
Charles POULAIN <charles.probe@gmail.com>
2025-04-15 18:25:12 +0200

    Update: Dockerfile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fastify = require('fastify')({ logger: true });
 
 fastify.register(require('./routes/user'));
 
-fastify.listen({ port: 5432 }, (err, address) => {
+fastify.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
 	if (err) {
 		fastify.log.error(err);
 		process.exit(1);
