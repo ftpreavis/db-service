a7597adfaef68149ffba4af7e0634681d1cf9f00
OnkelTag <jrmcheron@gmail.com>
2025-06-04 09:49:11 +0200

    ~|update settings with mode and lang

---
+++ prisma/migrations/20250604074558_update_settings/migration.sql
@@ -0,0 +1,21 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_UserSettings" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "background" TEXT,
+    "paddle" TEXT,
+    "ball" TEXT,
+    "divider" TEXT,
+    "score" TEXT,
+    "darkMode" BOOLEAN NOT NULL DEFAULT false,
+    "lang" TEXT NOT NULL DEFAULT 'ENGLISH',
+    CONSTRAINT "UserSettings_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_UserSettings" ("background", "ball", "darkMode", "divider", "id", "lang", "paddle", "score", "userId") SELECT "background", "ball", "darkMode", "divider", "id", "lang", "paddle", "score", "userId" FROM "UserSettings";
+DROP TABLE "UserSettings";
+ALTER TABLE "new_UserSettings" RENAME TO "UserSettings";
+CREATE UNIQUE INDEX "UserSettings_userId_key" ON "UserSettings"("userId");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
